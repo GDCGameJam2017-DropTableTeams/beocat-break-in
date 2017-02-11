48939703ee031eec6bb00b133921de2e0d0a38d9
Kyle Eisenbarger <keisenb@ksu.edu>
2017-02-11 07:07:09 -0600

    Finished ItemSeeder

--- database/seeds/ItemsSeeder.php
+++ database/seeds/ItemsSeeder.php
@@ -14,13 +14,67 @@ class ItemsSeeder extends Seeder
     public function run()
     {
 
-      $location1 = Locations::where('id', 1)->first();
-      $item1 = new Items;
-      $item1->name = "Leaf Blower";
-      $item1->properties = "POOP";
-      $item1->environment_interaction = 1;
-      $item1->location()->associate($location1);
-      $item1->save();
+      $locations = Locations::get(); //do a get() here instead of a first() then loop through
+
+      $values = array(0, 4, 8, 9, 10, 12, 13);
+      $one = array('name' => 'Leaf Blower', 'properties' => 'test', 'environment_interaction' => 0);
+      $five = array('name' => 'Formula One Car', 'properties' => 'test', 'environment_interaction' => 0);
+      $nine = array('name' => 'Beocat', 'properties' => 'test', 'environment_interaction' => 0);
+      $ten = array('name' => 'Laptop', 'properties' => 'test', 'environment_interaction' => 0);
+      $eleven = array('name' => 'Networking Cables', 'properties' => 'test', 'environment_interaction' => 0);
+      $thirteen = array('name' => 'Soldering Iron', 'properties' => 'test', 'environment_interaction' => 0);
+      $fourteen = array('name' => 'Air Vents', 'properties' => 'test', 'environment_interaction' => 0);
+
+
+
+        $item1 = new Items;
+        $item1->name = $one['name'];
+        $item1->properties = $one['properties'];
+        $item1->environment_interaction = $one['environment_interaction'];
+        $item1->location()->associate($locations[$values[0]]);
+        $item1->save();
+
+        $item2 = new Items;
+        $item2->name = $five['name'];
+        $item2->properties = $five['properties'];
+        $item2->environment_interaction = $five['environment_interaction'];
+        $item2->location()->associate($locations[$values[1]]);
+        $item2->save();
+
+        $item3 = new Items;
+        $item3->name = $nine['name'];
+        $item3->properties = $nine['properties'];
+        $item3->environment_interaction = $nine['environment_interaction'];
+        $item3->location()->associate($locations[$values[2]]);
+        $item3->save();
+
+        $item4 = new Items;
+        $item4->name = $ten['name'];
+        $item4->properties = $ten['properties'];
+        $item4->environment_interaction = $ten['environment_interaction'];
+        $item4->location()->associate($locations[$values[3]]);
+        $item4->save();
+
+        $item5 = new Items;
+        $item5->name = $eleven['name'];
+        $item5->properties = $eleven['properties'];
+        $item5->environment_interaction = $eleven['environment_interaction'];
+        $item5->location()->associate($locations[$values[4]]);
+        $item5->save();
+
+        $item6 = new Items;
+        $item6->name = $thirteen['name'];
+        $item6->properties = $thirteen['properties'];
+        $item6->environment_interaction = $thirteen['environment_interaction'];
+        $item6->location()->associate($locations[$values[5]]);
+        $item6->save();
+
+        $item7 = new Items;
+        $item7->name = $fourteen['name'];
+        $item7->properties = $fourteen['properties'];
+        $item7->environment_interaction = $fourteen['environment_interaction'];
+        $item7->location()->associate($locations[$values[6]]);
+        $item7->save();
 
     }
 }
